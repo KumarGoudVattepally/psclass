99e59f92b197b61f3ac260fc8d67370d1c4b3c72
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-11-06 12:12:38 +0530

    switch

--- index.js
+++ index.js
@@ -1,26 +1,100 @@
-// day = 1;
-// switch (day) {
-//   case 1:
-//     console.log("monday");
+// // // day = 1;
+// // // switch (day) {
+// // //   case 1:
+// // //     console.log("monday");
+// // //     break;
+// // //   case 2:
+// // //     console.log("tuesday");
+// // //     break;
+// // //   case 3:
+// // //     console.log("wensday");
+// // //     break;
+// // //   case 4:
+// // //     console.log("thurday");
+// // //     break;
+// // //   case 5:
+// // //     console.log("friday");
+// // //     break;
+// // //   case 6:
+// // //     console.log("saturday");
+// // //     break;
+// // //   case 7:
+// // //     console.log("sunday");
+// // //     break;
+// // //   default:
+// // //     console.log("invalid number");
+// // // }
+// // // let num = "8";
+// // // switch (true) {
+// // //   case 0:
+// // //     console.log("zero");
+// // //     break;
+// // //   case (num>0):
+// // //     console.log("+ve");
+// // //     break;
+// // //   case (num<0):
+// // //     console.log("-ve");
+// // //     break;
+// // //   default:
+// // //     console.log("nan");
+// // // }
+// // let operation = "delete";
+// // person = "admin";
+// // switch (person) {
+// //   case "admin":
+// //     switch (operation) {
+// //       case "delete":
+// //         {
+// //           console.log(" deleted suf");
+// //         }
+// //         break;
+// //       case "update":
+// //         console.log("update");
+// //         break;
+// //     }
+// // break;
+// //   case "user":
+// //     switch (operation) {
+// //       case "delete":
+// //         console.log("u r authorized");
+// //         break;
+// //       case " update":
+// //         console.log("upated");
+// //         break;
+// //     }
+// //   default: {
+// //     console.log("invaild");break;
+// //   }
+// // }
+// num = 3;
+// switch (true) {
+//   case num % 2 == 0:
+//     console.log("even");
 //     break;
-//   case 2:
-//     console.log("tuesday");
+//   case num % 2 !== 0:
+//     console.log("odd");
 //     break;
-//   case 3:
-//     console.log("wensday");
+//   default: {
+//     console.log("invaild");
 //     break;
-//   case 4:
-//     console.log("thurday");
-//     break;
-//   case 5:
-//     console.log("friday");
-//     break;
-//   case 6:
-//     console.log("saturday");
-//     break;
-//   case 7:
-//     console.log("sunday");
-//     break;
-//   default:
-//     console.log("invalid number");
+//   }
 // }
+let card = "sbi";
+amount = 10000;
+switch (amount) {
+  case 10000:
+    switch (card) {
+      case "hdfc":
+        console.log("10%");
+        break;
+      case "sbi":
+        console.log("5%");
+        break;
+      default:
+        console.log("noo");
+    }
+    break;
+  default:
+    console.log("no  10k");
+    break;
+}
