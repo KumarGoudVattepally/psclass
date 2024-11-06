4cdfef6addf0d62013ff8eee66e202475edd9e2b
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-11-06 11:31:53 +0530

    switch

---
+++ index.js
@@ -0,0 +1,26 @@
+// day = 1;
+// switch (day) {
+//   case 1:
+//     console.log("monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wensday");
+//     break;
+//   case 4:
+//     console.log("thurday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   case 6:
+//     console.log("saturday");
+//     break;
+//   case 7:
+//     console.log("sunday");
+//     break;
+//   default:
+//     console.log("invalid number");
+// }
