83293485c623e162685cd3176743cde9dbebe761
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-11-14 13:04:15 +0530

    example

---
+++ ps1.js
@@ -0,0 +1,46 @@
+// let num=1;
+// count=1
+// while(num<=100&&count<=10){
+//     if(num%8==0){
+//         console.log(num);
+//         count++
+
+//     }
+//     num++
+// }
+
+// let num=6
+// fac=1
+// for(i=num;i>=1;i--){
+//     fac=fac*i
+// }console.log(fac);
+/////////////////////////////////////////////////
+// let num = 987645;
+// even = 0;
+// odd = 0;
+
+// while (num > 0) {
+//   let dig = num % 10;
+//   if (dig % 2 == 0) {
+//     even++;
+//   } else {
+//     odd++;
+//   }
+//   num = Math.floor(num / 10);
+// }
+// console.log(even, odd);
+///////////////////////////////////////////////////
+// let num = 6736326789;
+// ev = 0;
+// od = 0;
+// while (num > 0) {
+//   let dig = num % 10;
+//   if (dig % 2 == 0) {
+//     ev = ev * 10 + dig;
+//   } else {
+//     od = od * 10 + dig;
+//   }
+//   num = Math.floor(num/10);
+// }
+// console.log(ev,od);
+/////////////////////////////////////////////////////////////////////////////////////////////////
